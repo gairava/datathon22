f6e95467359cf79a971a9b3441956fe8327cd115
Gabrielle Anne Irava <gabrielleanneirava@gmail.com>
2022-10-09 07:54:29 -0500

    second

--- login2.js
+++ login2.js
@@ -1,11 +1,11 @@
 function validate() {
-    var username=document.getElementById("username").ariaValueMax;
-    var password=document.getElementById("password").ariaValueMax;
-}
-if (username == "admin" && password=="1234"){
-    alert("login successful");
-    return false;
-}
-else {
-    alert("login failed");
+    var username=document.getElementById("username");
+    var password=document.getElementById("password");
+    if (username && password){
+        alert("login successful");
+        window.open("website.html");
+    }
+    else {
+        alert("login failed");
+    }
 }
